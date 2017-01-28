ca735b224f51ee05bfcf8a8fbe2b7ad35c35d891
Nonso Chukwuogor <nonso4yoo@gmail.com>
2017-01-28 11:58:30 +0100

    Added more recepients and added BCC

--- mail.php
+++ mail.php
@@ -4,10 +4,11 @@ $string = "Good Day,\r\nI am a young, result driven, goal oriented software engi
 $message = wordwrap($string, 70, "\r\n");
 $subject = "Chinonso Chukwuogor (About Me : Seniour Developer Applicant)";
 // $to = "nonso4yoo@gmail.com, Jay Dev<jasondev191@gmail.com>";
-$to = "riomachi@gmail.com, Doctor Fox <doctorfox29@yahoo.co.uk>";
+$to = "riomachi@gmail.com, Doctor Fox <doctorfox29@yahoo.co.uk>, obanor.chukwuwezam@gmail.com";
 $headers = 'From: nonso4yoo@gmail.com' . "\r\n" .
     'Reply-To: jasondev191@gmail.com' . "\r\n" .
     'cc: nonso4yoo@gmail.com' . "\r\n" .
+    'bcc: CEO Prepclass <wezam@prepclassng.com>' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 mail($to, $subject, $message, $headers);
 
